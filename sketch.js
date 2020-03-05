d1cd846150cc9e20135366861d7e4c8b3fd9278d
Dmitry G. Anderson <dandgerson@gmail.com>
2020-03-05 21:56:28 +0300

    canvas sketch applications

--- sketch.js
+++ sketch.js
@@ -1,26 +1,31 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: [2048, 2048],
+  // units: 'cm',
+  // orientation: 'landscape',
+  // pixelsPerInch: 300,
 };
 
 const sketch = () => {
-  return ({ context: c, width, height }) => {
+  return ({ context: c, width: w, height: h }) => {
+    // console.log({w, h})
+
     c.fillStyle = 'grey';
-    c.fillRect(0, 0, width, height);
+    c.fillRect(0, 0, w, h);
 
     c.beginPath()
     c.arc(
-      width / 2, // x
-      height / 2, // y
-      200, // radius
+      w * 0.5, // x
+      h * 0.5, // y
+      w * 0.2, // radius
       0, // start angle
       Math.PI * 2, // end angle
       false, // anticlockwise
     )
     c.fillStyle = 'salmon';
     c.fill()
-    c.lineWidth = 20
+    c.lineWidth = w * 0.01
     c.strokeStyle = 'black'
     c.stroke()
   };
