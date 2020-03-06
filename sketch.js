915e63212dc5eb39b68efbb460b24834fe0a1894
Dmitry G. Anderson <dandgerson@gmail.com>
2020-03-06 17:11:05 +0300

    color palettes

--- sketch.js
+++ sketch.js
@@ -1,28 +1,44 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random')
 const { lerp } = require('canvas-sketch-util/math')
+const palettes = require('nice-color-palettes')
 
 const settings = {
   dimensions: [2048, 2048],
 };
 
 const sketch = () => {
+  const colorCount = random.rangeFloor(1, 6)
+  const palette = random.shuffle(random.pick(palettes))
+    .slice(0, colorCount)
+
   const genGrid = (count) => {
     const points = []
-    
+
     const iterable = Array(count).fill(null)
     iterable.forEach((_, x) => {
       iterable.forEach((_, y) => {
         const xPos = count <= 1 ? 0.5 : x / (count - 1)
         const yPos = count <= 1 ? 0.5 : y / (count - 1)
-        
-        points.push([xPos, yPos])
+
+        points.push({
+          color: random.pick(palette),
+          radius: Math.abs(random.gaussian() * 0.0035),
+          position: [xPos, yPos],
+        })
       })
     })
     return points
   }
 
-  random.setSeed('Dmitry G. Anderson')
+  const seeds = [
+    'Daria A. Platonova',
+    'Dmitry G. Anderson',
+    'dandgerson',
+    'Maxim P. Pavlov',
+  ]
+
+  random.setSeed(seeds[2])
   const grid = genGrid(65).filter(() => random.value() > 0.5)
   const margin = 200
 
@@ -30,17 +46,20 @@ const sketch = () => {
     c.fillStyle = 'grey'
     c.fillRect(0, 0, w, h)
 
-    grid.forEach(([xPos, yPos]) => {
+    grid.forEach(({
+      position: [xPos, yPos],
+      radius,
+      color,
+    }) => {
       const x = lerp(margin, w - margin, xPos)
       const y = lerp(margin, h - margin, yPos)
 
       c.beginPath()
-      c.arc(x, y, 10, 0, Math.PI * 2, false)
-      c.strokeStyle = 'black'
-      c.lineWidth = 5
-      c.stroke()
+      c.arc(x, y, radius * w, 0, Math.PI * 2, false)
+      c.fillStyle = color
+      c.fill()
     })
-    
+
   };
 };
 
