859bbb71a014f7c6fabaf91a05bd16e210af66aa
Dmitry G. Anderson <dandgerson@gmail.com>
2020-03-06 15:01:32 +0300

    linear interpolation

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const canvasSketch = require('canvas-sketch');
+const { lerp } = require('canvas-sketch-util/math')
 
 const settings = {
   dimensions: [2048, 2048],
@@ -7,28 +8,29 @@ const settings = {
 const sketch = () => {
   const genGrid = (count) => {
     const points = []
-
+    
     const iterable = Array(count).fill(null)
     iterable.forEach((_, x) => {
       iterable.forEach((_, y) => {
         const xPos = count <= 1 ? 0.5 : x / (count - 1)
         const yPos = count <= 1 ? 0.5 : y / (count - 1)
-
+        
         points.push([xPos, yPos])
       })
     })
     return points
   }
-
+  
   const grid = genGrid(7)
+  const margin = 200
 
   return ({ context: c, width: w, height: h }) => {
     c.fillStyle = 'grey'
     c.fillRect(0, 0, w, h)
 
     grid.forEach(([xPos, yPos]) => {
-      const x = xPos * w
-      const y = yPos * h
+      const x = lerp(margin, w - margin, xPos)
+      const y = lerp(margin, h - margin, yPos)
 
       c.beginPath()
       c.arc(x, y, 100, 0, Math.PI * 2, false)
